fe11f298e6fc082d1b9b41989ed7c5153dab3061
Johannes Møgster <johannes.mogster@mojotech.no>
2021-07-01 10:14:09 +0200

    stiffie param callback use x,y,z

--- franka_example_controllers/src/cartesian_impedance_example_controller.cpp
+++ franka_example_controllers/src/cartesian_impedance_example_controller.cpp
@@ -213,14 +213,19 @@ void CartesianImpedanceExampleController::complianceParamCallback(
     franka_example_controllers::compliance_paramConfig& config,
     uint32_t /*level*/) {
   cartesian_stiffness_target_.setIdentity();
-  cartesian_stiffness_target_.topLeftCorner(3, 3)
-      << config.translational_stiffness_x * Eigen::Matrix3d::Identity();
+  cartesian_stiffness_target_(0, 0) = config.translational_stiffness_x;
+  cartesian_stiffness_target_(1, 1) = config.translational_stiffness_y;
+  cartesian_stiffness_target_(2, 2) = config.translational_stiffness_z;
+  
   cartesian_stiffness_target_.bottomRightCorner(3, 3)
       << config.rotational_stiffness * Eigen::Matrix3d::Identity();
   cartesian_damping_target_.setIdentity();
+  
   // Damping ratio = 1
-  cartesian_damping_target_.topLeftCorner(3, 3)
-      << 2.0 * sqrt(config.translational_stiffness_x) * Eigen::Matrix3d::Identity();
+  cartesian_damping_target_(0, 0) = 2.0 * sqrt(config.translational_stiffness_x);
+  cartesian_damping_target_(1, 1) = 2.0 * sqrt(config.translational_stiffness_y);
+  cartesian_damping_target_(2, 2) = 2.0 * sqrt(config.translational_stiffness_z);
+  
   cartesian_damping_target_.bottomRightCorner(3, 3)
       << 2.0 * sqrt(config.rotational_stiffness) * Eigen::Matrix3d::Identity();
   nullspace_stiffness_target_ = config.nullspace_stiffness;
